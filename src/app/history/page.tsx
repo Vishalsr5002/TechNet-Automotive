50246828a1c06964c3ac722ae85b170b1e8e5e5b
Vishalsr5002 <vishalrajasekaran123@gmail.com>
2026-02-26 18:15:02 +0530

    Refactor HistoryPage layout and history retrieval
    Updated the HistoryPage component to improve layout and functionality, including user history retrieval and display.

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,14 +1,51 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function HistoryPage() {
+  const router = useRouter();
+  const [history, setHistory] = useState<any[]>([]);
+  useEffect(() => {
+    const user = localStorage.getItem("currentUser");
+    if (!user) {
+      router.push("/");
+      return;
+    }
+    const historyKey = `history_${user}`;
+    const storedHistory = JSON.parse(
+      localStorage.getItem(historyKey) || "[]"
+    );
+    setHistory(storedHistory);
+  }, []);
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-10 rounded-xl shadow-md text-center">
-        <h1 className="text-2xl font-bold text-blue-800 mb-4">
-          History Page
-        </h1>
-        <p className="text-gray-500">
-          Your recent activity will appear here.
-        </p>
-      </div>
+    <div className="min-h-screen bg-gray-200 p-10">
+      <h1 className="text-3xl font-bold text-blue-900 mb-8">
+        Watch History
+      </h1>
+      {history.length === 0 ? (
+        <div className="bg-white p-10 text-gray-800 rounded-xl shadow text-center">
+          No recently viewed products.
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {history.map((item, index) => (
+            <div
+              key={`${item.slug}-${index}`}
+              onClick={() => router.push(`/products/${item.slug}`)}
+              className="bg-gray-50 border-b-5 border-blue-300 rounded-xl shadow hover:shadow-lg transition cursor-pointer w-80 h-60">
+              <img
+                src={item.img}
+                alt={item.title}
+                className="w-full h-40 object-cover rounded-t-xl"/>
+              <div className="p-4">
+                <h3 className="font-semibold text-gray-800">
+                  {item.title}
+                </h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
