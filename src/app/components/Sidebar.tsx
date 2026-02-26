9cbce72701865bc98a7762b5dd0853a1d8a5ec1e
Vishalsr5002 <vishalrajasekaran123@gmail.com>
2026-02-26 18:14:23 +0530

    Refactor Sidebar component with new icons and styles

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,34 +1,41 @@
 "use client";
+
+import { useRouter } from "next/navigation";
 import {
   HomeIcon,
   WrenchScrewdriverIcon,
   LanguageIcon,
 } from "@heroicons/react/24/solid";
-import { Power } from "lucide-react";
-//import { DynamicIcon } from 'lucide-react/dynamic';
-//const page = () => (
-  //<DynamicIcon name="camera" color="red" size={48} />);
+import { Power, ShareIcon } from "lucide-react";
+
 export default function Sidebar() {
+  const router = useRouter();
+  const handleLogout = () => {
+    localStorage.removeItem("currentUser");
+    router.push("/");
+  };
   return (
-    <aside className="w-64 bg-gray-700 text-white shadow-xl h-screen p-6 flex flex-col">
-      <ul className="space-y-6 text-gray-300">
-        
+    <aside className="w-64 bg-gray-700 text-white shadow-xl h-screen-auto p-6 flex flex-col">
+      <ul className="space-y-6 text-gray-300">        
         <li className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-800 hover:text-white transition duration-400 cursor-pointer">
           <HomeIcon className="w-6 h-6" />
-          Dashboard 
+          Dashboard
         </li>
-
-        <li className="flex divide-amber-600 items-center gap-3 p-3 rounded-lg hover:bg-gray-800 hover:text-white transition duration-400 cursor-pointer">
+        <li className="flex items-center gap-2 p-3 rounded-lg hover:bg-gray-800 hover:text-white transition duration-400 cursor-pointer">
+          <ShareIcon className="w-6 h-6" />
+          Share Animation
+        </li>
+        <li className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-800 hover:text-white transition duration-400 cursor-pointer">
           <WrenchScrewdriverIcon className="w-6 h-6" />
           Settings
         </li>
-
         <li className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-800 hover:text-white transition duration-400 cursor-pointer">
           <LanguageIcon className="w-6 h-6" />
           Languages
         </li>
-
-        <li className="flex items-left gap-3 p-3 rounded-lg hover:bg-gray-800 hover:text-white transition duration-400 cursor-pointer">
+        <li
+          onClick={handleLogout}
+          className="flex items-center gap-3 p-3 rounded-lg hover:bg-gray-800 hover:text-white transition duration-300 cursor-pointer">
           <Power className="w-6 h-6" />
           Logout
         </li>
